b1157524c7375d42db52e490c5946ba9abbfab27
DarOrange <naranjapi22@gmai.com>
2026-07-05 15:30:42 -0600

    Finalize Core creation flow: Opportunity to Creature pipeline stable

---
+++ test/from core.opportunity.py
@@ -0,0 +1,38 @@
+from core.opportunity.opportunity_factory import OpportunityFactory
+from core.creature.creature_factory import CreatureFactory
+
+
+class FakeSpecies:
+    """
+    Species mínima para test sin depender de todo el sistema.
+    """
+    def __init__(self):
+        self.name = "pikachu"
+        self.default_form = "base"
+
+
+def test_core_opportunity_to_creature_flow():
+    # Arrange
+    species = FakeSpecies()
+
+    # Act
+    opportunity = OpportunityFactory.create(species)
+
+    creature = CreatureFactory.create(
+        creature_id=1,
+        trainer_id="trainer_1",
+        opportunity=opportunity
+    )
+
+    # Assert - identidad base
+    assert creature.species == opportunity.species
+    assert creature.variant == opportunity.variant
+    assert creature.ivs == opportunity.ivs
+    assert creature.size == opportunity.size
+    assert creature.nature == opportunity.nature
+    assert creature.is_shiny == opportunity.is_shiny
+    assert creature.current_form == opportunity.initial_form
+
+    # Assert - ownership
+    assert creature.trainer_id == "trainer_1"
+    assert creature.id == 1
