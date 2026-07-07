b6ca8e1297f455da156fdb06f94b54371940cb26
DarOrange <naranjapi22@gmail.com>
2026-07-07 16:42:39 -0600

    Update Discord image source to use PokéAPI GIF collection

--- interfaces/discord/images.py
+++ interfaces/discord/images.py
@@ -1,6 +1,6 @@
 from interfaces.discord.mapeo_pokes import obtener_id_gif
 
-BASE_GIF_URL = "https://pub-23cb564f6c174627926c1ac0409563d4.r2.dev/gifs_calidad"
+BASE_GIF_URL = "https://pub-23cb564f6c174627926c1ac0409563d4.r2.dev"
 
 
 def get_species_gif(
@@ -8,6 +8,9 @@ def get_species_gif(
     shiny: bool,
 ) -> str:
     folder = "shiny" if shiny else "regular"
-    gif_id = obtener_id_gif(species_id)
 
-    return f"{BASE_GIF_URL}/{folder}/{gif_id}.gif"
+    if species_id <= 898:
+        return f"{BASE_GIF_URL}/gifs_pokeapi/{folder}/{species_id}.gif"
+
+    gif_id = obtener_id_gif(species_id)
+    return f"{BASE_GIF_URL}/gifs_calidad/{folder}/{gif_id}.gif"
