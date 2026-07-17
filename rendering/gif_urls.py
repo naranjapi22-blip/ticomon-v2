3d317e6a81fa3b8b327e7a31b8fe780eb3a990ed
DarOrange <naranjapi22@gmai.com>
2026-07-17 11:19:59 -0600

    Gifs

--- rendering/gif_urls.py
+++ rendering/gif_urls.py
@@ -1,19 +1,6 @@
-from urllib.parse import parse_qsl, urlencode, urlsplit, urlunsplit
-
 BASE_GIF_URL = "https://pub-23cb564f6c174627926c1ac0409563d4.r2.dev"
-GIF_ASSET_VERSION = "20260717-1"
 
 
 def version_gif_url(url: str) -> str:
-    parts = urlsplit(url)
-    query = dict(parse_qsl(parts.query, keep_blank_values=True))
-    query["v"] = GIF_ASSET_VERSION
-    return urlunsplit(
-        (
-            parts.scheme,
-            parts.netloc,
-            parts.path,
-            urlencode(query),
-            parts.fragment,
-        )
-    )
+    """Return the GIF URL without cache-version query parameters."""
+    return url.split("?", maxsplit=1)[0]
